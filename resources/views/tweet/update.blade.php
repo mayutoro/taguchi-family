462d0feeb7a12cd1d2b41f97d23a5706f5a1cb67
taguchi <taguchi@nss-net.co.jp>
2024-01-24 15:55:51 +0900

    削除処理の追加

--- resources/views/tweet/update.blade.php
+++ resources/views/tweet/update.blade.php
@@ -9,13 +9,15 @@
     <title>つぶやきアプリ</title>
 </head>
 <body>
-<h1>つぶやきアプリ</h1>
+<h1>つぶやきを編集する</h1>
 <div>
+    <a href="{{ route('tweet.index') }}">< 戻る</a>
     <p>投稿フォーム</p>
     @if (session('feedback.success'))
         <p style="color: green;">{{ session('feedback.success') }}</p>
     @endif
-    <form action="{{ route('tweet.create') }}" method="post">
+    <form action="{{ route('tweet.update.put', ['tweetId' => $tweet->id]) }}" method="post">
+        @method('put')
         @csrf
         <label for="tweet-content">つぶやき</label>
         <span>140文字まで</span>
@@ -23,23 +25,8 @@
         @error('tweet')
         <p style="color: red;">{{ $message }}</p>
         @enderror
-        <button type="submit">投稿</button>
+        <button type="submit">編集</button>
     </form>
-</div>
-<div>
-@foreach ($tweets as $tweet)
-    <details>
-        <summary>{{ $tweet->content }}</summary>
-        <div>
-            <a href="{{ route('tweet.update.index', ['tweetId' => $tweet->id]) }}">編集</a>
-            <form action="{{ route('tweet.delete', ['tweetId' => $tweet->id]) }}" method="post">
-                @method('DELETE')
-                @csrf
-                <button type="submit">削除</button>
-            </form>
-        </div>
-    </details>
-@endforeach
 </div>
 </body>
 </html>
