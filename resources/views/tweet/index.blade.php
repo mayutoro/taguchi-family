72f36d61172dbe90b2ec06ffb0df036d825d957e
taguchi <taguchi@nss-net.co.jp>
2024-01-26 13:18:39 +0900

    ViteからMixへ切り替え

--- resources/views/tweet/index.blade.php
+++ resources/views/tweet/index.blade.php
@@ -6,6 +6,8 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0,
           maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+    <script src="{{ mix('/js/app.js') }}" defer></script>
     <title>つぶやきアプリ</title>
 </head>
 <body>
